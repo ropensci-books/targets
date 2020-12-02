b3722a0e0fef1f06ed162b84fee2035b55069bbd
wlandau <will.landau@gmail.com>
2020-12-02 01:16:53 -0500

    Add workflow step for manual

--- inst/publish.sh
+++ inst/publish.sh
@@ -1,7 +1,6 @@
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
 [ -z "${GITHUB_PAT}" ] && exit 0
-Rscript -e 'bookdown::render_book(input = "index.Rmd", output_format = "bookdown::gitbook")'
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/wlandau/targets-manual.git gh-pages
 cd gh-pages
 ls -a | grep -Ev "^\.$|^..$|^\.git$" | xargs rm -rf
