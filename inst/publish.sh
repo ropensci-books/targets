3e214f04d5f665f5c39ded5843668b9beab8c546
wlandau <will.landau@gmail.com>
2021-01-12 10:19:27 -0500

    Update URLs

--- inst/publish.sh
+++ inst/publish.sh
@@ -1,7 +1,7 @@
 git config --global user.email "will.landau@gmail.com"
 git config --global user.name "wlandau"
 [ -z "${GITHUB_PAT}" ] && exit 0
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/wlandau/targets-manual.git gh-pages
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/ropensci-books/targets.git gh-pages
 cd gh-pages
 ls -a | grep -Ev "^\.$|^..$|^\.git$" | xargs rm -rf
 cp -r ../_book/* ./
