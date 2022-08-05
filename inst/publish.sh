b8a5ab7bfc7c389093680dec6aa93e6dd4fbcb40
wlandau <will.landau@gmail.com>
2022-08-05 10:42:23 -0400

    GHA user

--- inst/publish.sh
+++ inst/publish.sh
@@ -1,5 +1,5 @@
-git config --global user.email "will.landau@gmail.com"
-git config --global user.name "wlandau"
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
 [ -z "${GITHUB_PAT}" ] && exit 0
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/ropensci-books/targets.git gh-pages
 cd gh-pages
